80404d62a1f80ec9369557d087770f8ab9926bfd
MANGA-1614 <johncbao1@gmail.com>
2021-01-13 15:02:04 -0500

    Updated to L11-Challenge

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -41,7 +41,9 @@ app.component('product-display', {
 
       </div>
     </div>
-  </div>`,
+    <review-form @review-submitted="addReview"></review-form>
+    <review-list v-if="reviews.length" :reviews="reviews"></review-list>
+    </div>`,
   data() {
     return {
         product: 'Socks',
@@ -51,7 +53,8 @@ app.component('product-display', {
         variants: [
           { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
           { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-        ]
+        ],
+        reviews: []
     }
   },
   methods: {
@@ -60,8 +63,11 @@ app.component('product-display', {
       },
       updateVariant(index) {
           this.selectedVariant = index
-      }
-  },
+      },
+      addReview(review) {
+          this.reviews.push(review)
+      }      
+    },
   computed: {
       title() {
           return this.brand + ' ' + this.product
